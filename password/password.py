56eeecb30d27778850a901aec563928e5ff6b32f
danielgallagher8 <51876055+danielgallagher8@users.noreply.github.com>
2021-05-07 10:53:00 +0100

    Update password.py

--- password/password.py
+++ password/password.py
@@ -1,1 +1,17 @@
+"""
+Password Keeper application to store passwords locally
+"""
 
+#Import libraries
+from PyQt5 import QtWidgets
+import sys
+
+#Define classes
+class App(QtWidgets.QMainWindow):
+  
+  def __init__(self, *args, **kwargs):
+    super(App, self).__init__(*args, **kwargs)
+    self.initial_layout()
+  
+  def initial_layout(self):
+    pass
